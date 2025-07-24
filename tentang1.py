54aab4ccc36226c3d948e4b0bbb984239962834b
salmaapriliahudaputri <89372828+salmaapriliahudaputri@users.noreply.github.com>
2025-07-24 20:51:40 +0700

    Update tentang1.py

--- tentang1.py
+++ tentang1.py
@@ -605,9 +605,9 @@ def app():
                 tn, fp, fn, tp = cm.ravel()
 
                 accuracy = (tp + tn) / (tp + tn + fp + fn)
-                precision = tp / (tp + fp) if (tp + fp) > 0 else 0
-                recall = tp / (tp + fn) if (tp + fn) > 0 else 0
-                f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+                precision = tp / (tp + fp) 
+                recall = tp / (tp + fn)
+                f1 = 2 * (precision * recall) / (precision + recall)
 
                 metrics = {
                     'Accuracy': accuracy,
